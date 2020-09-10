0b4cdaef3671caac90bfe4a228ad58ec46ee993b
Sébastien Langis <sebastien.langis@gmail.com>
2020-09-10 16:43:11 -0400

    Module 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,6 +4,16 @@
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
+    <hr>
+    <h2>
+      Filtered Books By Ownership
+    </h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{ filter }}</option>
+    </select>
+    <ul>
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
+    </ul>
     <br>
     <hr>
     <book-form @addBook='appendBook'></book-form>
@@ -11,6 +21,7 @@
 </template>
 
 <script>
+import _ from "lodash";
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -20,10 +31,12 @@ export default {
     return {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
+      filters: ["bought", "borrowed"],
+      holding: "bought",
       books: [
-        {title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true},
-        {title: "American Gods", author: "Neil Gaiman", finishedReading: false},
-        {title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true}
+        {title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownership: 'borrowed'},
+        {title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: 'bought'},
+        {title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true, ownership: 'borrowed'}
       ]
     };
   },
@@ -31,12 +44,18 @@ export default {
     BookItem,
     BookForm
   },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding])
+    }
+  },
   methods: {
     appendBook(bookData) {
       this.books.push({
         title: bookData.bookTitle,
         author: bookData.bookAuthor,
         finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership,
       });
     }
   }
