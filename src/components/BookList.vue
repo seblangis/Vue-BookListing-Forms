ccbc0d7291aea75c053d8a084bd0ecac4e880801
Sébastien Langis <sebastien.langis@gmail.com>
2020-09-10 16:25:12 -0400

    Module 2

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
-    <h1>{{title}}</h1>
+    <h1>{{ title }}</h1>
     <ul>
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
     </ul>
-    <br><hr>
+    <br>
+    <hr>
     <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
@@ -20,9 +21,9 @@ export default {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
-        { title: "American Gods", author: "Neil Gaiman" },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman" }
+        {title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true},
+        {title: "American Gods", author: "Neil Gaiman", finishedReading: false},
+        {title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true}
       ]
     };
   },
@@ -31,8 +32,12 @@ export default {
     BookForm
   },
   methods: {
-    appendBook(bookTitle, bookAuthor) {
-      this.books.push({ title: bookTitle, author: bookAuthor });
+    appendBook(bookData) {
+      this.books.push({
+        title: bookData.bookTitle,
+        author: bookData.bookAuthor,
+        finishedReading: bookData.finishedReading,
+      });
     }
   }
 };
@@ -43,6 +48,7 @@ h1,
 h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
